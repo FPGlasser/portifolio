fb0b1f14e5e546b8d2e5d4562c39d3fd3b212713
FP Glasser <glasserpaulo95@hotmail.com>
2023-07-16 16:24:53 -0300

    chore: consifigurações iniciais e estrutura de pastas

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="bg-ui-bg font-poppins flex min-h-screen flex-col items-center">
+      <div className="max-w-[1100px] w-full">
+
+      </div>
+    </main>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors:{
+      "ui":{
+        "bg": "#040633",
+        "primary": "#172D67",
+        "secondary": "#2E73EA",
+        "text": {
+          "primary": "D9D9D9",
+          "secondary": "F9D9C9",
+          "tertiary": "#BCBCBC"
+        },
+
+        "other": {
+          "1": "#FF3233",
+          "2": "#FEBE0B",
+          "3": "#8C15E9",
+          "4": "#25d366",
+          "5": "rgba(4, 6, 51, 0.44)"
+        }
+      }
+      
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
